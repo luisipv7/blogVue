23b301e6b1a6e71d3e6d9f7515b7f250e64aed84
Luis Fernando Flores dos Santos (GOVBR - Porto Alegre) <Luis.Santos@cetil.com.br>
2020-03-22 23:12:50 -0300

    Adicionando categorias

--- backend/api/user.js
+++ backend/api/user.js
@@ -51,5 +51,14 @@ module.exports = app => {
       .then(users => res.json(users))
       .catch(err => res.status(500).send(err))
   }
-  return { save, get }
+  const getById = (req, res) => {
+    const id = req.params.id
+    app.db('users')
+      .select('id', 'name', 'email', 'admin')
+      .where({ id })
+      .first()
+      .then(user => res.json(user))
+      .catch(err => res.status(500).send(err))
+  }
+  return { save, get, getById }
 }
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,8 +1,18 @@
 module.exports = app => {
-  app.route('/users')
+  app.route('/categorys')
     .post(app.api.user.save)
     .get(app.api.user.get)
 
   app.route('/users/:id')
     .put(app.api.user.save)
+    .get(app.api.user.getById)
+
+  app.route('/categories')
+    .post(app.api.category.save)
+    .get(app.api.category.get)
+
+  app.route('/categories/:id')
+    .put(app.api.category.save)
+    .get(app.api.category.getById)
+    .delete(app.api.category.remove)
 }
